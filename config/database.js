a762c341989f19ae65d0ae0ddefc8cbe89b85a52
Boli27 <judiemu27@gmail.com>
2024-11-25 19:49:02 -0500

    Añadir vista frontend para despliegue

--- config/database.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize(process.env.DB_NAME || 'ScrapingWeb', process.env.DB_USER||'root', process.env.DB_PASS||'', {
-  host: process.env.DB_HOST||'localhost',
-  dialect: 'mysql',
-});
-
-module.exports = sequelize;
--- routes/verRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { getVisualizationData } = require('../controllers/vistaController');
-const router = express.Router();
-
-router.get('/ver', getVisualizationData);
-
-module.exports = router;
